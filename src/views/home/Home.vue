c759443e5692d239fc8c94c139c1593635164f1c
xiaohu <1355830196@qq.com>
2021-02-25 09:54:26 +0800

    版本1

---
+++ src/views/home/Home.vue
@@ -0,0 +1,175 @@
+!<template>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommend"/>
+    <feature-view></feature-view>
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']"></tab-control>
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar.vue'
+import HomeSwiper from './childComps/HomeSwiper'
+import RecommendView from './childComps/RecommendView'
+import FeatureView from './childComps/FeatureView.vue'
+
+
+
+import {getHomeMultidata} from "network/home"
+import TabControl from '../../components/content/tabControl/TabControl.vue'
+
+export default {
+  name: "Home",
+  components:{
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl
+  },
+  data(){
+      return{
+        banners:[],
+        recommend:[],
+        goods:{
+          'pop':{page:0, list:[]},
+          'news':{page:0, list:[]},
+          'sell':{page:0, list:[]}
+        }
+      }
+  },
+  created(){
+    getHomeMultidata().then(res =>{
+      console.log(res);
+      this.banners = res.data.banner.list,
+      this.recommend = res.data.recommend.list
+    })
+  }
+
+}
+</script>
+
+<style>
+#home{
+  padding-top: 44px;
+}
+.home-nav{
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control{
+  background-color: #fff;
+  position: sticky;
+  top: 44px;
+}
+
+</style>
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,45 @@
+!<template>
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    recommends:{
+      type:Array,
+      default(){
+        return[]
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+.recommend{
+  display: flex;
+  font-size: 14px;
+  text-align: center;
+  padding: 10px 0 20px;
+  border-bottom: 8px solid #eee;
+}
+.recommend-item{
+  flex: 1;
+  margin-bottom: 10px;
+  
+
+}
+.recommend-item img{
+ width: 100px;
+ height: 100px;
+}
+
+</style>
